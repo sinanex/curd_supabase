a5ac777b86337f54c50cebe78f3497bb2fd302d2
sinanex <ms4730435@gmail.com>
2024-12-25 15:08:05 +0530

    model updated

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -3,14 +3,16 @@ class todoModel {
   String? title;
   String? subtitle;
 
-  todoModel({
-    required this.id,
-    required this.subtitle,
-    required this.title
-  });
+  todoModel({required this.id, required this.subtitle, required this.title});
 
-  factory todoModel.fromJson(Map<String,dynamic>json){
-    return todoModel(id: json['id'], subtitle: json['subtitle'], title: json['title']);
+  factory todoModel.fromJson(Map<String, dynamic> json) {
+    return todoModel(
+        id: json['id'], subtitle: json['subtitle'], title: json['title']);
   }
-  Map<String,dynamic>
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'subtitle': subtitle,
+    };
+  }
 }
